57c1d8ad72f41b737153527e27426e2a7760dcf0
Wojciech Słomczyński <w.sl@o2.pl>
2018-10-02 17:15:56 +0200

    implement basics of Farm

---
+++ src/Components/Farm/Farm.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Farm.scss';
+
+class Farm extends Component {
+  render() {
+    return (
+       <div>farmm</div>
+    );
+  }
+}
+
+export default Farm;
