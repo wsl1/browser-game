a6dde192805b2c72231a768c6d79b4023b72da70
Wojciech Słomczyński <w.sl@o2.pl>
2018-10-02 17:13:59 +0200

    add BrowserRouter, Routes and Navbar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter, Route, Switch} from 'react-router-dom';
+import Farm from './Components/Farm/Farm';
+import Shop from './Components/Shop/Shop';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="app">
+
+        <BrowserRouter>
+          <div>
+            <Navbar />
+            <Switch>
+              <Route path="/farm" component={Farm}/>
+              <Route path="/shop" component={Shop}/>
+            </Switch>
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
