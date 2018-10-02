93a6d4e8e7ea10c4d1e3e756a45a3ce8a78e15ef
Wojciech Słomczyński <w.sl@o2.pl>
2018-10-02 17:14:31 +0200

    setup basic configuration for redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {createStore, combineReducers} from 'redux';
+import {Provider} from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const reducer = (state = {}, action) => {
+    return state;
+}
+const store = createStore(reducer, {})
+
+
+ReactDOM.render(
+    <Provider store={store}><App /></Provider>,
+    document.getElementById('root'));
 registerServiceWorker();
