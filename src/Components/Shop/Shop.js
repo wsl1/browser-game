e8bda3cb2c5e6719b8987a4ba2adf461ce023aa5
Wojciech Słomczyński <w.sl@o2.pl>
2018-10-02 17:16:21 +0200

    implement basics of Shop

---
+++ src/Components/Shop/Shop.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Shop.scss';
+
+class Shop extends Component {
+  render() {
+    return (
+       <div>shop</div>
+    );
+  }
+}
+
+export default Shop;
