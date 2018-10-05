9a1a5f854fa8dc26099370b1f18a75152fec2715
Wojciech Słomczyński <w.sl@o2.pl>
2018-10-05 17:06:28 +0200

    render Shopcards

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -1,12 +1,46 @@
 import React, { Component } from 'react';
 import './Shop.scss';
+import {connect} from 'react-redux';
+import ShopCard from '../ShopCard/ShopCard';
+import Items from './Items.json';
+import {buyCow, buyChicken} from '../../Actions/FarmActions';
 
 class Shop extends Component {
+  
   render() {
+    const shopCards = Items.map((shopCard) => {
+      let purchase;
+      switch(shopCard.name) {
+        case 'Cow':
+          purchase = this.props.buyCow;
+          break;
+        case 'Chicken':
+          purchase = this.props.buyChicken
+      }
+      return (
+        <ShopCard
+          name={shopCard.name}
+          price={shopCard.price}
+          purchase={purchase}
+        />
+      );
+    });
+
     return (
-       <div>shop</div>
+       <div className="shop">
+          {shopCards}
+       </div>
     );
   }
 }
 
-export default Shop;
+const mapDispatchToProps = (dispatch) => ({
+  buyCow: () => {
+    dispatch(buyCow())
+  },
+  buyChicken: () => {
+    dispatch(buyChicken())
+  }
+});
+
+export default connect(null, mapDispatchToProps)(Shop);
