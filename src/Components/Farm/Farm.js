70f587f9cab7890c87c455da71c57c35a6b840cc
Wojciech Słomczyński <w.sl@o2.pl>
2018-10-06 16:45:37 +0200

    add sidebar

--- src/Components/Farm/Farm.js
+++ src/Components/Farm/Farm.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 import './Farm.scss';
+import FarmStats from '../FarmStats/FarmStats';
+import SideBar from '../SideBar/SideBar';
+import SideBarItem from '../SideBarItem/SideBarItem';
 
 class Farm extends Component {
   render() {
     return (
-       <div>farmm</div>
+       <div className="farm">
+          <SideBar>
+            <SideBarItem text="Animals" link="/farm" />
+            <SideBarItem text="Incomes" link="/farm"/>
+          </SideBar>
+          <FarmStats />
+       </div>
     );
   }
 }
