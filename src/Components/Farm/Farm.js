0f855ace5c182b93b6b89afe1bce93ae93661bef
Wojciech Słomczyński <w.sl@o2.pl>
2018-10-09 17:04:44 +0200

    add routes

--- src/Components/Farm/Farm.js
+++ src/Components/Farm/Farm.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
 import './Farm.scss';
-import FarmStats from '../FarmStats/FarmStats';
 import SideBar from '../SideBar/SideBar';
 import SideBarItem from '../SideBarItem/SideBarItem';
+import {Route} from 'react-router-dom';
+import FarmAnimals from '../FarmAnimals/FarmAnimals';
+import FarmIncomes from '../FarmIncomes/FarmIncomes';
 
 class Farm extends Component {
   render() {
     return (
        <div className="farm">
           <SideBar>
-            <SideBarItem text="Animals" link="/farm" />
-            <SideBarItem text="Incomes" link="/farm"/>
+            <SideBarItem text="Animals" link="/farm/animals" />
+            <SideBarItem text="Incomes" link="/farm/incomes"/>
           </SideBar>
-          <FarmStats />
+          <Route exact path="/farm/animals" component={FarmAnimals}/>
+          <Route exact path="/farm/incomes" component={FarmIncomes} />  
        </div>
     );
   }
 }
 
 export default Farm;
+//<FarmStats />
