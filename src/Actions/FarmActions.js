6de25403a95ab2049df1e734152ec36e59fd1597
Wojciech Słomczyński <w.sl@o2.pl>
2018-10-09 21:49:04 +0200

    add sell actions

--- src/Actions/FarmActions.js
+++ src/Actions/FarmActions.js
@@ -1,9 +1,19 @@
-export const buyCow = () => ({
-    type: "BUY_COW",
-    payload: 1500
+export const buyCows = (quantity) => ({
+    type: "BUY_COWS",
+    payload: quantity * 1500
 });
 
-export const buyChicken = () => ({
-    type: "BUY_CHICKEN",
-    payload: 20
+export const buyChickens = (quantity) => ({
+    type: "BUY_CHICKENS",
+    payload: quantity * 20
+});
+
+export const sellCows = (quantity) => ({
+    type: "SELL_COWS",
+    payload: quantity * 1500
+});
+
+export const sellChickens = (quantity) => ({
+    type: "SELL_CHICKENS",
+    payload: quantity * 20
 });
