71e15092a2ca0c8168a840e90763595858f2ed54
Wojciech Słomczyński <w.sl@o2.pl>
2018-10-09 21:52:26 +0200

    add component

---
+++ src/Components/FarmAnimals/FarmAnimals.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class FarmAnimals extends Component {
+  render() {
+    return (
+       <div className="farm-animals">
+        
+       </div>
+    );
+  }
+}
+
+export default FarmAnimals;
