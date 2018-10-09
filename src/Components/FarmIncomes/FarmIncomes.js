d20cb1eecc2d94e717aa95d3455b202b9afb1bd2
Wojciech Słomczyński <w.sl@o2.pl>
2018-10-09 21:52:34 +0200

    add component

---
+++ src/Components/FarmIncomes/FarmIncomes.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Proptypes from 'proptypes'
+import {connect} from 'react-redux';
+
+class FarmIncomes extends Component {
+  render() {
+    return (
+       <div>
+        incomes
+       </div>
+    );
+  }
+}
+
+export default FarmIncomes;
