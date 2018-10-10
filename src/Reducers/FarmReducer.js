31cf0425e9f108e73dfc06056767a476b0fb2535
Wojciech Słomczyński <w.sl@o2.pl>
2018-10-10 13:15:09 +0200

    add SELL_COWS and SELL_CHICKENS actions

--- src/Reducers/FarmReducer.js
+++ src/Reducers/FarmReducer.js
@@ -1,24 +1,49 @@
-export default (state = {
-    balance: 10000,
-    cows: 0,
-    chickens: 0,
-}, 
-action) => {
-    if(state.balance - action.payload < 0) return state;
-    switch(action.type) {
-        case 'BUY_COWS':
-            state = {
-                ...state,
-                cows: ++state.cows,
-                balance: state.balance - action.payload
-            }
-            break;
-        case 'BUY_CHICKENS':
-            state = {
-                ...state,
-                chickens: ++state.chickens,
-                balance: state.balance - action.payload
-            }
-    }
-    return state;
+export default (
+    state = {
+        balance: 10000,
+        animals: {
+            cows: 0,
+            chickens: 0,
+        }
+    }, action) => {
+        const { price, quantity } = {...action.payload};
+        const { balance } = {...state};
+        const animals = {
+            ...state.animals,                    
+        };
+        let updatedBalance;
+
+        switch(action.type) {
+            case 'BUY_COWS':
+                if(state.balance - (quantity * price) < 0) return state;      
+                animals.cows = animals.cows + quantity;
+                updatedBalance = balance - (price * quantity);
+                state = {
+                    ...state,
+                    animals,
+                    updatedBalance
+                };
+                break;
+            case 'BUY_CHICKENS':
+                if(state.balance - (quantity * price) < 0) return state;
+                animals.chickens = animals.chickens + quantity;
+                updatedBalance = balance - (price * quantity);
+                state = {
+                    ...state,
+                    animals,
+                    updatedBalance
+                };
+                break;
+            case 'SELL_COWS':
+                if(state.animals.cows - quantity < 0) return state;
+                animals.cows = animals.chickens - quantity;
+                updatedBalance = balance + (price * quantity);
+                state = {
+                    ...state,
+                    animals,
+                    updatedBalance
+                };
+                break;
+        }
+        return state;
 }
