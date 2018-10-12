4cb301b404d7ee4eeb397240dbf32f25cfa01020
Wojciech Słomczyński <w.sl@o2.pl>
2018-10-12 13:04:38 +0200

    implement component

--- src/Components/ShopCard/ShopCard.js
+++ src/Components/ShopCard/ShopCard.js
@@ -4,13 +4,59 @@ import Proptypes from 'proptypes'
 import {connect} from 'react-redux';
 
 class ShopCard extends Component {
+
+  state = {
+    quantity: 1
+  }
+
+  resetState = () => this.setState({quantity: 1})
+
+  incrementQuantity = () => {
+    this.setState({
+      ...this.state,
+      quantity: ++this.state.quantity
+    });
+  }
+
+  decrementQuantity = () => {
+    const state = {...this.state};
+    if(!(state.quantity === 1)) {
+      this.setState({
+        ...this.state,
+        quantity: --this.state.quantity
+      });
+    }
+  }
+
+  onClick = () => {
+    this.props.onSubmit(this.state.quantity);
+    this.resetState();
+  }
+
   render() {
-    const {name, price} = this.props;
+    const {name, price, type} = this.props;
+    let buttonText;
+    switch(type) {
+      case 'buy':
+        buttonText = 'Buy';
+        break;
+      case 'sell':
+        buttonText = 'Sell';
+        break;
+    }
     return (
        <div className="shop-card">
             <p className="shop-card__name">{name}</p>
             <p className="shop-card__price">${price}</p>
-            <button className="shop-card__purchase" onClick={this.props.purchase}>Purchase</button>
+            <div className="shop-card__quantity-picker">
+              <div className="shop-card__quantity">{this.state.quantity}</div>
+              <div className="shop-card__buttons">
+                <button onClick={this.incrementQuantity}>+</button>
+                <button onClick={this.decrementQuantity}>-</button>
+              </div>
+
+            </div>
+            <button className="shop-card__submit" onClick={this.onClick}>{buttonText}</button>
        </div>
     );
   }
@@ -19,7 +65,8 @@ class ShopCard extends Component {
 ShopCard.propTypes = {
     name: Proptypes.string,
     price: Proptypes.number,
-    purchase: Proptypes.func
+    type: Proptypes.oneOf(['buy', 'sell']),
+    onSubmit: Proptypes.func,
 }
 
 export default ShopCard;
